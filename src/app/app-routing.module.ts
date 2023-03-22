27ab61e2bd003f7d4c8ef63488d9bd4c7c8c9523
milagros.negrin <adriana.negrin.17@gmail.com>
2023-03-22 19:52:34 +0100

    folder para desplegar en gitHub pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FormComponent} from "./features/git/components/form/form.component";
+import {GitComponent} from "./features/git/git.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'component-one', pathMatch:    'full' },
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: GitComponent
+      }
+    ]
+  },
   { path: 'gitcommand', loadChildren: () => import('./features/git/git.module').then(m => m.GitModule) },
   { path: 'gitcreate', component: FormComponent }
 ];
